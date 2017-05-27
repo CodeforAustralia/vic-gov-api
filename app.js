5c0644c3b307aa0589c3ff8f59ec87515b173187
rexmortus <alecs@eml.cc>
2017-05-27 21:58:36 +1000

    Butts lol

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true,
 }));
 app.listen(4000);
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+console.log('Running a GraphQL API server at localhost:4000/graphql butts lol');
